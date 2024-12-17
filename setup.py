c550e86a8cab32eb996773501a9063877bccdbd6
th-schmidt <thomas.schmidt@uni-mannheim.de>
2024-12-17 11:12:42 +0100

    Update to version 0.3.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name="dygest",
     author="Thomas Schmidt",
-    version='0.3.1',
+    version='0.3.2',
     packages=find_packages(),
     license="MIT",
     description="DYGEST: Document Insights Generator",
